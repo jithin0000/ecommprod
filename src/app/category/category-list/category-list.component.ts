ac32b8b048282fe062de035b41d1c6a7cf3d4c97
jithin0000 <mbjithinbabu@gmail.com>
2020-03-18 21:10:26 +0530

    category filter by name completed

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CategoryService } from 'src/app/services/category/category.service';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Category } from 'src/app/models/category.model';
+import { filter, tap, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent implements OnInit, OnDestroy {
+  
 
   categories$: Observable<Category[]>
+  searchTerm$  = new  Subject<String>()
+  autoCompleteList$: Observable<Category[]>
+
+  
 
   constructor(
     private categoryService: CategoryService
@@ -20,6 +26,23 @@ export class CategoryListComponent implements OnInit {
 
     this.categories$ = this.categoryService.getAll()
     this.categories$.subscribe(console.log)
+
+   this.autoCompleteList$ =  this.searchTerm$.pipe(
+      debounceTime(250),
+      distinctUntilChanged(),
+      switchMap( va => this.categoryService.filterByName(va.toString()))
+    )
+  }
+
+  searchName(name: HTMLInputElement){
+   
+    const value = name.value
+
+    this.searchTerm$.next(value)
+  }
+
+  ngOnDestroy(): void {
+    this.searchTerm$.unsubscribe()
   }
 
 }
--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from '../base/base.service';
 import { Category } from 'src/app/models/category.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,14 @@ export class CategoryService extends BaseService<Category> {
 
   constructor(http: HttpClient) {
     super(http, "/category")
+   }
+
+   filterByName(name: string){
+     return this.httpClient.get<Category[]>(this.urls+"/name",{
+       params: new HttpParams()
+       .append("search", name)
+     }).pipe(
+       catchError(this.handlePromise)
+     )
    }
 }
